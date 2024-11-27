26fc3fd920736debfc7d84bf205f465e69b177d9
Zeyafb <beainizozalt@gmail.com>
2024-11-26 21:44:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ def main():
     st.title("Practice Exam Simulator")
 
     # Handle query parameters
-    query_params = st.experimental_get_query_params()
+    query_params = st.query_params
     part_name = query_params.get("part", [None])[0]
     question_number = query_params.get("question", [None])[0]
     if question_number is not None:
